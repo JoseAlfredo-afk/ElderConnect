8ff54aed5cc5693b88975f428a45350695515c85
brunoeduardoed433 <brunoeduardoed433@gmail.com>
2026-06-17 17:10:04 -0300

    feat: criando a pagina de cadastrar usuario

--- ElderConnect-Web/src/app/views/account/sign-up/sign-up.ts
+++ ElderConnect-Web/src/app/views/account/sign-up/sign-up.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './sign-up.html',
-  styleUrl: './sign-up.css',
+  styleUrl: './sign-up.css'
 })
-export class SignUp {}
+export class SignUp { }
