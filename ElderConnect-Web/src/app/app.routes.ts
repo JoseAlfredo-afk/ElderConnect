7b24c19dba83e70417ee38390e723c888d1b03e6
brunoeduardoed433 <brunoeduardoed433@gmail.com>
2026-06-17 15:33:54 -0300

    feat: criacao da pagina de login

--- ElderConnect-Web/src/app/app.routes.ts
+++ ElderConnect-Web/src/app/app.routes.ts
@@ -1,16 +1,19 @@
 import { Routes } from '@angular/router';
 import { SignIn } from './views/account/sign-in/sign-in';
 import { SignUp } from './views/account/sign-up/sign-up';
-import { MyProfile } from './views/account/my-profile/my-profile';
-import { Help } from './views/pages/help/help';
 import { Home } from './views/pages/home/home';
-import { Main } from './views/pages/main/main';
-import { UserList } from './views/pages/user/user-list/user-list';
-import { UserEdit } from './views/pages/user/user-edit/user-edit';
-import { UserDetail } from './views/pages/user/user-detail/user-detail';
 import { NotFound } from './views/not-found/not-found';
 
 export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: Home
+    },
     {
         path: 'account/sign-in',
         component: SignIn
@@ -18,34 +21,6 @@ export const routes: Routes = [
     {
         path: 'account/sign-up',
         component: SignUp
-    },
-    {
-        path: 'account/my-profile',
-        component: MyProfile
-    },
-    {
-        path: 'help',
-        component: Help
-    },
-    {
-        path: 'home',
-        component: Home
-    },
-    {
-        path: 'main',
-        component: Main
-    },
-    {
-        path: 'user/list',
-        component: UserList
-    },
-    {
-        path: 'user/edit',
-        component: UserEdit
-    },
-    {
-        path: 'user/detail',
-        component: UserDetail
     },
     {
         path: '**',
