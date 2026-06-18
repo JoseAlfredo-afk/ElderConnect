f808e21c24ffb844ac7e5804679d35c4fb1bda10
brunoeduardoed433 <brunoeduardoed433@gmail.com>
2026-06-18 12:01:09 -0300

    feat: mockando a acao de logar e cadastrar para apresentação do pid

--- ElderConnect-Web/src/app/views/account/sign-in/sign-in.ts
+++ ElderConnect-Web/src/app/views/account/sign-in/sign-in.ts
@@ -1,10 +1,35 @@
+// ... seus outros imports ...
+import { CommonModule } from '@angular/common'; // Garanta que o CommonModule está nos imports do @Component
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
-  imports: [RouterLink], 
+  standalone: true,
+  imports: [RouterLink, FormsModule, CommonModule],
   templateUrl: './sign-in.html',
   styleUrl: './sign-in.css'
 })
-export class SignIn { }
+export class SignIn {
+  emailInput: string = '';
+  senhaInput: string = '';
+  mostrarSucesso: boolean = false;
+
+  constructor(private router: Router) { }
+
+  logar(event: Event) {
+    event.preventDefault();
+
+    if (this.emailInput.trim() !== '' && this.senhaInput.trim() !== '') {
+      this.mostrarSucesso = true;
+
+      setTimeout(() => {
+        this.router.navigate(['/home']);
+      }, 2500);
+      
+    } else {
+      alert('Por favor, preencha o e-mail e a senha de simulação.');
+    }
+  }
+}
