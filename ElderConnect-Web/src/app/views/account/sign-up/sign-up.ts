16b9ad4951a149798d5652ff87cacfb5a5577aac
brunoeduardoed433 <brunoeduardoed433@gmail.com>
2026-06-19 13:54:13 -0300

    feat: criacao da pagina de cadastro do cuidador e adiciona estado reativo para rastrear se o usuario e idoso ou cuidador.

--- ElderConnect-Web/src/app/views/account/sign-up/sign-up.ts
+++ ElderConnect-Web/src/app/views/account/sign-up/sign-up.ts
@@ -1,69 +1,68 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink, Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { AuthService } from '../../../services/user/auth';
+import { AuthService } from '../../../services/user/auth'; 
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [RouterLink, ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, RouterLink],
   templateUrl: './sign-up.html',
   styleUrl: './sign-up.css'
 })
 export class SignUp {
-  cadastroForm: FormGroup;
-  mostrarSucesso: boolean = false;
-  private authService = inject(AuthService)
+  tipoConta: 'idoso' | 'cuidador' = 'idoso';
 
-  constructor(private fb: FormBuilder, private router: Router) {
-    this.cadastroForm = this.fb.group({
-      nome: ['', [Validators.required, Validators.minLength(3)]],
-      dataNascimento: ['', [Validators.required]],
-      email: ['', [Validators.required, Validators.email]],
-      cpf: ['', [Validators.required]],
-      telefone: ['', [Validators.required]],
-      senha: ['', [Validators.required, Validators.minLength(6)]],
-      confirmaSenha: ['', [Validators.required]]
-    });
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
+  protected authService = inject(AuthService);
+
+  cadastroForm: FormGroup = this.fb.group({
+    nome: ['', [Validators.required]],
+    dataNascimento: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    cpf: ['', [Validators.required]],
+    telefone: ['', [Validators.required]],
+    senha: ['', [Validators.required, Validators.minLength(6)]],
+    confirmaSenha: ['', [Validators.required]]
+  });
+
+  alterarTipoConta(tipo: 'idoso' | 'cuidador') {
+    this.tipoConta = tipo;
   }
 
-  aplicarMascara(event: Event, tipo: string) {
+  aplicarMascara(event: Event, tipo: 'data' | 'cpf' | 'telefone') {
     const input = event.target as HTMLInputElement;
     let valor = input.value.replace(/\D/g, '');
 
-    if (tipo === 'cpf') {
-      if (valor.length > 11) valor = valor.substring(0, 11);
-      valor = valor.replace(/(\d{3})(\d)/, '$1.$2');
-      valor = valor.replace(/(\d{3})(\d)/, '$1.$2');
-      valor = valor.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+    if (tipo === 'data') {
+      if (valor.length > 2) valor = valor.substring(0, 2) + '/' + valor.substring(2);
+      if (valor.length > 5) valor = valor.substring(0, 5) + '/' + valor.substring(5, 9);
+    } else if (tipo === 'cpf') {
+      if (valor.length > 3) valor = valor.substring(0, 3) + '.' + valor.substring(3);
+      if (valor.length > 7) valor = valor.substring(0, 7) + '.' + valor.substring(7);
+      if (valor.length > 11) valor = valor.substring(0, 11) + '-' + valor.substring(11, 13);
+    } else if (tipo === 'telefone') {
+      if (valor.length > 0) valor = '(' + valor;
+      if (valor.length > 3) valor = valor.substring(0, 3) + ') ' + valor.substring(3);
+      if (valor.length > 10) valor = valor.substring(0, 10) + '-' + valor.substring(10, 14);
     }
-
-    else if (tipo === 'data') {
-      if (valor.length > 8) valor = valor.substring(0, 8);
-      valor = valor.replace(/(\d{2})(\d)/, '$1/$2');
-      valor = valor.replace(/(\d{2})(\d{1,4})$/, '$1/$2');
-    }
-
-    else if (tipo === 'telefone') {
-      if (valor.length > 11) valor = valor.substring(0, 11);
-      valor = valor.replace(/^(\d{2})(\d)/g, '($1) $2');
-      valor = valor.replace(/(\d{5})(\d)/, '$1-$2');
-    }
-
     input.value = valor;
-
     this.cadastroForm.get(tipo === 'data' ? 'dataNascimento' : tipo)?.setValue(valor, { emitEvent: false });
   }
 
   submeter() {
     if (this.cadastroForm.valid) {
-      console.log('Dados enviados para simulação (Mock):', this.cadastroForm.value);
-    
-      this.authService.mostrarAlertaCadastroGlobal = true; 
-
-      this.router.navigate(['/account/sign-in']);
-
+      console.log('Dados do cadastro base:', this.cadastroForm.value);
+      
+      if (this.tipoConta === 'cuidador') {
+      
+        this.router.navigate(['/account/complete-profile']);
+      } else {
+        this.authService.mostrarAlertaCadastroGlobal = true; 
+        this.router.navigate(['/account/sign-in']);
+      }
     } else {
       this.cadastroForm.markAllAsTouched();
     }
