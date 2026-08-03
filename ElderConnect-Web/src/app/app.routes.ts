6a4ce3572f3a2fccc3f00d998371db3fb9116b9e
brunoeduardoed433 <brunoeduardoed433@gmail.com>
2026-08-03 11:39:31 -0300

    feat: criacao da estrutura da tela de buscar cuidadores

--- ElderConnect-Web/src/app/app.routes.ts
+++ ElderConnect-Web/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { SignIn } from './views/account/sign-in/sign-in';
 import { SignUp } from './views/account/sign-up/sign-up';
 import { Home } from './views/pages/home/home';
 import { CompleteProfile } from './views/account/sign-up-caregiver/sign-up-caregiver';
-import { ElderDashboard } from './views/pages/elder-dashboard/elder-dashboard';
+import { ElderDashboard } from './views/pages/elder-dashboard/elder';
 
 export const routes: Routes = [
     {
