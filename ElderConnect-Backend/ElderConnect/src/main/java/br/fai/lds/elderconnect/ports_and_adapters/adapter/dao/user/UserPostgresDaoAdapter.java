7781d27932896cd97e52bc57b1ed0c796ec9fc32
Marcela <marcela.rezende21@gmail.com>
2026-08-26 01:32:03 -0300

    feat: adicao do adapter para Postgres

---
+++ ElderConnect-Backend/ElderConnect/src/main/java/br/fai/lds/elderconnect/ports_and_adapters/adapter/dao/user/UserPostgresDaoAdapter.java
@@ -0,0 +1,43 @@
+package br.fai.lds.elderconnect.ports_and_adapters.adapter.dao.user;
+
+import br.fai.lds.elderconnect.domain.UserModel;
+import br.fai.lds.elderconnect.ports_and_adapters.port.dao.user.UserDao;
+
+import java.util.List;
+
+public class UserPostgresDaoAdapter implements UserDao {
+    @Override
+    public int add(UserModel entity) {
+        return 0;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public UserModel readyById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<UserModel> readAll() {
+        return List.of();
+    }
+
+    @Override
+    public void updateInformation(int id, UserModel entity) {
+
+    }
+
+    @Override
+    public UserModel readByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public boolean updatePassword(int id, String password) {
+        return false;
+    }
+}
