db2365d4904803eb540247966cee0e5fe391e95d
Marcela <marcela.rezende21@gmail.com>
2026-08-27 16:22:34 -0300

    fix: adicao de atributo e mudança de nome de UpdateProfileDto

--- ElderConnect-Backend/ElderConnect/src/main/java/br/fai/lds/elderconnect/dto/UpdateProfileDto.java
+++ ElderConnect-Backend/ElderConnect/src/main/java/br/fai/lds/elderconnect/dto/UpdateProfileDto.java
@@ -7,15 +7,17 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class UpdateFullnameDto {
+public class UpdateProfileDto {
 
     private int id;
     private String fullname;
+    private String phoneNumber;
 
     public UserModel toUserModel() {
         final UserModel userModel = new UserModel();
         userModel.setId(id);
         userModel.setFullname(fullname);
+        userModel.setPhoneNumber(phoneNumber);
         return userModel;
     }
 }
