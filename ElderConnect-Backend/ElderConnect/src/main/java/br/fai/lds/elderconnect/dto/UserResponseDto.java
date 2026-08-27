b911d3dd12836149fe765f554d9aef7834eb0c24
Marcela <marcela.rezende21@gmail.com>
2026-08-27 16:31:14 -0300

    feat: adicao de devolver dados sem a senha

---
+++ ElderConnect-Backend/ElderConnect/src/main/java/br/fai/lds/elderconnect/dto/UserResponseDto.java
@@ -0,0 +1,31 @@
+package br.fai.lds.elderconnect.dto;
+
+import br.fai.lds.elderconnect.domain.UserModel;
+import br.fai.lds.elderconnect.domain.UserType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserResponseDto {
+
+    private int id;
+    private String fullname;
+    private String email;
+    private String phoneNumber;
+    private UserType userType;
+    private String birthDate;
+
+    public static UserResponseDto toUserModel(UserModel userModel) {
+        UserResponseDto dto = new UserResponseDto();
+
+        dto.setId(userModel.getId());
+        dto.setFullname(userModel.getFullname());
+        dto.setEmail(userModel.getEmail());
+        dto.setPhoneNumber(userModel.getPhoneNumber());
+        dto.setUserType(userModel.getUserType());
+        dto.setBirthDate(userModel.getBirthDate());
+
+        return dto;
+    }
+}
