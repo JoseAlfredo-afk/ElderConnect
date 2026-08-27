1a83a4ee5234d9d9b1667112ae7453a0e1d44dfc
Marcela <marcela.rezende21@gmail.com>
2026-08-27 16:25:03 -0300

    feat: adicao da funcionalidade de atualizacao de email

--- ElderConnect-Backend/ElderConnect/src/main/java/br/fai/lds/elderconnect/ports_and_adapters/port/service/user/UserService.java
+++ ElderConnect-Backend/ElderConnect/src/main/java/br/fai/lds/elderconnect/ports_and_adapters/port/service/user/UserService.java
@@ -3,6 +3,6 @@ package br.fai.lds.elderconnect.ports_and_adapters.port.service.user;
 import br.fai.lds.elderconnect.domain.UserModel;
 import br.fai.lds.elderconnect.ports_and_adapters.port.service.crud.CrudService;
 
-public interface UserService extends CrudService<UserModel>, FindByEmailService, UpdatePasswordService, LoginService {
+public interface UserService extends CrudService<UserModel>, FindByEmailService, UpdatePasswordService, UpdateEmailService, LoginService {
 
 }
