f6bd4e45163b53ac5e10da82c0ee99627a5226ba
Marcela <marcela.rezende21@gmail.com>
2026-08-29 09:32:43 -0300

    feat: criacao do MedicationRestController

---
+++ ElderConnect-Backend/ElderConnect/src/main/java/br/fai/lds/elderconnect/controller/MedicationRestController.java
@@ -0,0 +1,15 @@
+package br.fai.lds.elderconnect.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/medications")
+public class MedicationRestController {
+
+
+
+
+
+
+}
