644fa4bab7efe270d9ab71785b12b6a0623d4643
Marcela <marcela.rezende21@gmail.com>
2026-09-10 15:29:19 -0300

    fix: adicao de atributos para UserResponseDto

--- ElderConnect-Backend/ElderConnect/src/main/java/br/fai/lds/elderconnect/dto/user/UserResponseDto.java
+++ ElderConnect-Backend/ElderConnect/src/main/java/br/fai/lds/elderconnect/dto/user/UserResponseDto.java
@@ -1,11 +1,13 @@
 package br.fai.lds.elderconnect.dto.user;
 
 import br.fai.lds.elderconnect.domain.UserModel;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserResponseDto {
 
     private int id;
@@ -14,6 +16,13 @@ public class UserResponseDto {
     private String phoneNumber;
     private String userType;
     private String birthDate;
+    private String availabilitySchedule;
+    private String streetAddress;
+    private String specialization;
+    private String city;
+    private String neighborhood;
+    private String experience;
+
 
     public static UserResponseDto fromUserModel(UserModel userModel) {
         UserResponseDto userResponseDto = new UserResponseDto();
@@ -24,6 +33,15 @@ public class UserResponseDto {
         userResponseDto.setPhoneNumber(userModel.getPhoneNumber());
         userResponseDto.setUserType(userModel.getUserType().name());
         userResponseDto.setBirthDate(userModel.getBirthDate());
+
+        if(userModel.getUserType() == UserModel.UserType.CUIDADOR){
+            userResponseDto.setAvailabilitySchedule(userModel.getAvailabilitySchedule());
+            userResponseDto.setStreetAddress(userModel.getStreetAddress());
+            userResponseDto.setSpecialization(userModel.getSpecialization());
+            userResponseDto.setCity(userModel.getCity());
+            userResponseDto.setNeighborhood(userModel.getNeighborhood());
+            userResponseDto.setExperience(userModel.getExperience());
+        }
 
         return userResponseDto;
     }
