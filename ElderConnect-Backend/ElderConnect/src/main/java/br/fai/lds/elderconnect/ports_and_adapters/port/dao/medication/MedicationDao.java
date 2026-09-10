3026e510ce6a6f6638f8cd16939100708f6aeefc
Marcela <marcela.rezende21@gmail.com>
2026-09-10 19:01:34 -0300

    feat: adicao das interfaces de Medicamento

---
+++ ElderConnect-Backend/ElderConnect/src/main/java/br/fai/lds/elderconnect/ports_and_adapters/port/dao/medication/MedicationDao.java
@@ -0,0 +1,7 @@
+package br.fai.lds.elderconnect.ports_and_adapters.port.dao.medication;
+
+import br.fai.lds.elderconnect.domain.Medication;
+import br.fai.lds.elderconnect.ports_and_adapters.port.dao.crud.CrudDao;
+
+public interface MedicationDao extends CrudDao<Medication>{
+}
---
+++ ElderConnect-Backend/ElderConnect/src/main/java/br/fai/lds/elderconnect/ports_and_adapters/port/service/medication/MedicationService.java
@@ -0,0 +1,7 @@
+package br.fai.lds.elderconnect.ports_and_adapters.port.service.medication;
+
+import br.fai.lds.elderconnect.domain.Medication;
+import br.fai.lds.elderconnect.ports_and_adapters.port.service.crud.CrudService;
+
+public interface MedicationService extends CrudService<Medication> {
+}
