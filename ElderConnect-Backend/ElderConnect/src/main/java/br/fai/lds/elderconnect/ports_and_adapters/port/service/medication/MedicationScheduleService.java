70c8204fd08ffd33270e51595b0a144f684c170b
Marcela <marcela.rezende21@gmail.com>
2026-09-14 16:45:11 -0300

    fix: correcao de nome de interface

--- ElderConnect-Backend/ElderConnect/src/main/java/br/fai/lds/elderconnect/ports_and_adapters/port/service/medication/MedicationScheduleService.java
+++ ElderConnect-Backend/ElderConnect/src/main/java/br/fai/lds/elderconnect/ports_and_adapters/port/service/medication/MedicationScheduleService.java
@@ -3,5 +3,5 @@ package br.fai.lds.elderconnect.ports_and_adapters.port.service.medication;
 import br.fai.lds.elderconnect.domain.MedicationSchedule;
 import br.fai.lds.elderconnect.ports_and_adapters.port.service.crud.CrudService;
 
-public interface MedicationScheduleService extends CrudService<MedicationSchedule>, FindBySeniorId {
+public interface MedicationScheduleService extends CrudService<MedicationSchedule>, FindBySeniorIdService {
 }
