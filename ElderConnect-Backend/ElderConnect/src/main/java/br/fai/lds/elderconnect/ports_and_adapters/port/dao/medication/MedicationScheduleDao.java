5f776a183e7e9e9caabdc99c372d854227ab8239
Marcela <marcela.rezende21@gmail.com>
2026-09-13 21:30:26 -0300

    feat: adicao das interfaces de Agenda de Medicamento

---
+++ ElderConnect-Backend/ElderConnect/src/main/java/br/fai/lds/elderconnect/ports_and_adapters/port/dao/medication/MedicationScheduleDao.java
@@ -0,0 +1,7 @@
+package br.fai.lds.elderconnect.ports_and_adapters.port.dao.medication;
+
+import br.fai.lds.elderconnect.domain.MedicationSchedule;
+import br.fai.lds.elderconnect.ports_and_adapters.port.dao.crud.CrudDao;
+
+public interface MedicationScheduleDao extends CrudDao<MedicationSchedule> {
+}
