c2c9ac8105c8f742be4448923e4f6088d600d748
Marcela <marcela.rezende21@gmail.com>
2026-09-14 13:19:34 -0300

    feat: adicao de interfaces de leitura de id de agenda de medicamentos

---
+++ ElderConnect-Backend/ElderConnect/src/main/java/br/fai/lds/elderconnect/ports_and_adapters/port/service/medication/FindBySeniorId.java
@@ -0,0 +1,10 @@
+package br.fai.lds.elderconnect.ports_and_adapters.port.service.medication;
+
+import br.fai.lds.elderconnect.domain.MedicationSchedule;
+
+import java.util.List;
+
+public interface FindBySeniorId {
+
+    public List<MedicationSchedule> findBySeniorId(final int seniorId);
+}
