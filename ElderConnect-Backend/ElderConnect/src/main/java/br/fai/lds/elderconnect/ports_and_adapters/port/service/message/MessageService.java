c57c11440dfcfa2c961ddc633394837cf79724d3
Marcela <marcela.rezende21@gmail.com>
2026-09-15 15:02:35 -0300

    fix: remocao de metodos não necessarios

--- ElderConnect-Backend/ElderConnect/src/main/java/br/fai/lds/elderconnect/ports_and_adapters/port/service/message/MessageService.java
+++ ElderConnect-Backend/ElderConnect/src/main/java/br/fai/lds/elderconnect/ports_and_adapters/port/service/message/MessageService.java
@@ -1,7 +1,9 @@
 package br.fai.lds.elderconnect.ports_and_adapters.port.service.message;
 
 import br.fai.lds.elderconnect.domain.Message;
+import br.fai.lds.elderconnect.ports_and_adapters.port.service.crud.CreateService;
 import br.fai.lds.elderconnect.ports_and_adapters.port.service.crud.CrudService;
+import br.fai.lds.elderconnect.ports_and_adapters.port.service.crud.FindService;
 
-public interface MessageService extends CrudService<Message>, FindConversationByUsersIdsService,FindByUserIdService {
+public interface MessageService extends CreateService<Message>, FindService<Message>, FindConversationByUsersIdsService,FindByUserIdService {
 }
