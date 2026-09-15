fbd4e9deb7beab2f8d3860de8887a7bac75bed02
Marcela <marcela.rezende21@gmail.com>
2026-09-15 19:37:19 -0300

    feat: adicao de interface para procurar cuidadores

---
+++ ElderConnect-Backend/ElderConnect/src/main/java/br/fai/lds/elderconnect/ports_and_adapters/port/service/user/FindCaregiversService.java
@@ -0,0 +1,11 @@
+package br.fai.lds.elderconnect.ports_and_adapters.port.service.user;
+
+import br.fai.lds.elderconnect.domain.UserModel;
+
+import java.util.List;
+
+
+public interface FindCaregiversService {
+
+    List<UserModel> findCaregivers();
+}
