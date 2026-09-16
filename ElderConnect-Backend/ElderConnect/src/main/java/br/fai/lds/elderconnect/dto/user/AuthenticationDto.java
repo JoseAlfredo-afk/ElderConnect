9b697f56d3dbdbfd0c77d69350e75036811354eb
Marcela <marcela.rezende21@gmail.com>
2026-09-15 21:30:33 -0300

    feat: adicao de AuthenticationDto

---
+++ ElderConnect-Backend/ElderConnect/src/main/java/br/fai/lds/elderconnect/dto/user/AuthenticationDto.java
@@ -0,0 +1,14 @@
+package br.fai.lds.elderconnect.dto.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AuthenticationDto {
+
+    private String email;
+    private String password;
+
+
+}
