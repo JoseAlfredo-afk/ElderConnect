5549bd386b507f9e0002e9c2f41a764320ebfa82
Marcela <marcela.rezende21@gmail.com>
2026-09-15 21:48:02 -0300

    feat: adicao de BasicAuthentication em AppConfiguration

--- ElderConnect-Backend/ElderConnect/src/main/java/br/fai/lds/elderconnect/configuration/AppConfiguration.java
+++ ElderConnect-Backend/ElderConnect/src/main/java/br/fai/lds/elderconnect/configuration/AppConfiguration.java
@@ -7,19 +7,34 @@ import br.fai.lds.elderconnect.ports_and_adapters.adapter.dao.medication.Medicat
 import br.fai.lds.elderconnect.ports_and_adapters.adapter.dao.message.MessagePostgresDaoAdapter;
 import br.fai.lds.elderconnect.ports_and_adapters.adapter.dao.user.UserFakeDaoAdapter;
 import br.fai.lds.elderconnect.ports_and_adapters.adapter.dao.user.UserPostgresDaoAdapter;
+import br.fai.lds.elderconnect.ports_and_adapters.adapter.service.security.BasicAuthenticationServiceAdapter;
 import br.fai.lds.elderconnect.ports_and_adapters.port.dao.contract.ContractDao;
 import br.fai.lds.elderconnect.ports_and_adapters.port.dao.medication.MedicationDao;
 import br.fai.lds.elderconnect.ports_and_adapters.port.dao.medication.MedicationScheduleDao;
 import br.fai.lds.elderconnect.ports_and_adapters.port.dao.message.MessageDao;
 import br.fai.lds.elderconnect.ports_and_adapters.port.dao.user.UserDao;
+import br.fai.lds.elderconnect.ports_and_adapters.port.service.security.AuthenticationService;
+import br.fai.lds.elderconnect.ports_and_adapters.port.service.user.UserService;
 import org.apache.catalina.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 
 import java.sql.Connection;
+import java.util.Arrays;
 
 @Configuration
 public class AppConfiguration {
+
+    private final Environment environment;
+
+    public AppConfiguration(Environment environment) {
+        this.environment = environment;
+        System.out.println("--------------------");
+        System.out.println("active profile: " + Arrays.toString(environment.getActiveProfiles()));
+        System.out.println("--------------------");
+    }
 
 
     public UserDao getUserFakeDao(){
@@ -49,6 +64,12 @@ public class AppConfiguration {
     @Bean
     public MessageDao getMessageDao(final Connection connection){
         return new MessagePostgresDaoAdapter(connection);
+    }
+
+    @Profile("basic")
+    @Bean
+    public AuthenticationService basicAuthenticationService(final UserService userService){
+        return new BasicAuthenticationServiceAdapter(userService);
     }
 
 
